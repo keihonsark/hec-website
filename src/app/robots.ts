5efaba2bf79c812af771d46acdaaaa045af8215b
keihonsark <keihonsark@gmail.com>
2026-05-08 10:45:37 -0700

    Add robots.ts pointing to sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: '*', allow: '/', disallow: ['/api/', '/thank-you'] }
+    ],
+    sitemap: 'https://www.homeenergyconstruction.com/sitemap.xml',
+  }
+}
